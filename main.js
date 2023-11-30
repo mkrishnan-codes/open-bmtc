d88348942bece1c2bf6879310925dc7d86643d9a
Manu Krishnan <manu.krishnan@falconx.io>
2023-11-30 14:12:46 +0530

    alpha ver search

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-import './style.css'
-import {BMTC_TABLE} from "./res/table";
-import './style.css'
-document.querySelector('#app').innerHTML = BMTC_TABLE;
-
+import "./style.css";
+import { BMTC_TABLE } from "./res/table";
+import "./style.css";
+import { addAttributes, assignColumnIdentifier, onFilter } from "./src/util";
+import { FILTER_ID, TABLE_ID } from "./src/constants";
+const init = () => {
+  document.querySelector("#app").innerHTML = BMTC_TABLE;
+  document.querySelector("#app table")?.setAttribute("id", TABLE_ID);
+  let input = addAttributes(document.createElement("input"), {
+    type: "text",
+    class: "bus-filter",
+    id: FILTER_ID,
+  });
+  input.addEventListener("input", onFilter);
+  document.querySelector("#app").prepend(input);
+  assignColumnIdentifier();
+};
+init();
