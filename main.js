72a549185e169a84083da46a54a1430296728608
Manu Krishnan <manu.krishnan@falconx.io>
2023-12-01 17:37:03 +0530

    env vars

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import "./style.css";
 import { BMTC_TABLE } from "./res/table";
 import "./style.css";
 import { addAttributes, createJsObjectFromTable, onFilter, createHtmlElementFromstring } from "./src/util";
-import { FILTER_ID, TABLE_ID } from "./src/constants";
+import { FILTER_ID, GITHUB_RUN_NUMBER, GITHUB_SHA_SHORT, TABLE_ID } from "./src/constants";
 import { content } from "./res/contentTemplate";
 const init = () => {
   document.querySelector("#app").innerHTML = BMTC_TABLE;
@@ -12,5 +12,6 @@ const init = () => {
   const input = document.getElementById(FILTER_ID)
   input.addEventListener("input", onFilter);
   createJsObjectFromTable();
+  document.querySelector("#app").append(createHtmlElementFromstring(`<span>${GITHUB_RUN_NUMBER}--${GITHUB_SHA_SHORT}</span>`))
 };
 init();
