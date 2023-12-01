d8b8d9c7659f246bf5aca08e6e2b583ff6afe016
Manu Krishnan <manu.krishnan@falconx.io>
2023-12-01 17:51:57 +0530

    git ver ADDED

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import "./style.css";
 import { BMTC_TABLE } from "./res/table";
 import "./style.css";
-import { addAttributes, createJsObjectFromTable, onFilter, createHtmlElementFromstring } from "./src/util";
-import { FILTER_ID, GITHUB_RUN_NUMBER, GITHUB_SHA_SHORT, TABLE_ID } from "./src/constants";
+import { createJsObjectFromTable, onFilter, createHtmlElementFromstring } from "./src/util";
+import { FILTER_ID, GITHUB_RUN_NUMBER, GITHUB_SHA, TABLE_ID } from "./src/constants";
 import { content } from "./res/contentTemplate";
 const init = () => {
   document.querySelector("#app").innerHTML = BMTC_TABLE;
@@ -12,6 +12,6 @@ const init = () => {
   const input = document.getElementById(FILTER_ID)
   input.addEventListener("input", onFilter);
   createJsObjectFromTable();
-  document.querySelector("#app").append(createHtmlElementFromstring(`<span>${GITHUB_RUN_NUMBER}--${GITHUB_SHA_SHORT}</span>`))
+  document.querySelector("#app").append(createHtmlElementFromstring(`<div class="d-flex pad">About :  Ver 0.${GITHUB_RUN_NUMBER} / Build${GITHUB_SHA}</div>`))
 };
 init();
--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const TABLE_ID = "bus-table";
 export const FILTER_ID = "bus-filter";
-export const GITHUB_SHA_SHORT=import.meta.env.VITE_GITHUB_SHA_SHORT;
+export const GITHUB_SHA=import.meta.env.VITE_GITHUB_SHA;
 export const GITHUB_RUN_NUMBER=import.meta.env.VITE_GITHUB_RUN_NUMBER;
 
